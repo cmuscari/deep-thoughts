c7a0bd051d037fb67001cad3ab77112e36cd1565
Cortnie <cortniemuscari@gmail.com>
2022-06-27 21:09:02 -0500

    mutations

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -16,10 +17,26 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+
+// With the configuration of authLink, we use the setContext() function to retrieve the token from localStorage and set the HTTP request headers of every request to include the token, whether the request needs it or not.
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+
+// we need to combine the authLink and httpLink objects so that every request retrieves the token and sets the request headers before making the request to the API. 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
+// *** To recap, we just created and implemented functionality that when a user signs up or logs in and receives an access token in return, we store it in localStorage. With this token, we can decode it to retrieve the logged-in user's nonsensitive data, check if the token is still valid, and use it to make requests to the server.  Any time we make a request to the server, we use the code we just implemented a few moments ago to automatically set the HTTP request headers with our token. This way, our server can receive the request, check the token's validity, and allow us to continue our request if it is.
 
 function App() {
   return (
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
+
+// set up our returning JSX to conditionally return different navigation items depending on the outcome of the Auth.loggedIn(). If it returns true, and we're logged in, we want to display navigation items tailored to the user. If it returns false, we'll display the default items for logging in and signing up.
 const Header = () => {
+
+  // We're not actually going to let that <a> carry out its duty to return to the root of the application. Instead, we're going to create a function that executes when clicked to run the .logout() method we created in the AuthService class. 
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <Link to="/">
           <h1>Deep Thoughts</h1>
         </Link>
-
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
